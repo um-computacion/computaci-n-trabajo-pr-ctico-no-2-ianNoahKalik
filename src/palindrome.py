fd42f2cff715d37ffe34fb322ecf02c11bfadd47
unknown <i.kalik@alumno.um.edu.ar>
2025-04-14 00:53:54 -0300

    fase 3 implementacion de texto

--- src/palindrome.py
+++ src/palindrome.py
@@ -1,3 +1,10 @@
 def is_palindrome(world):
     word = word.lower().replace(" ", "").replace(".", "").replace(",", "").replace(";", "").replace(":", "").replace("?", "").replace("!", "")
-    
+    if len(word) <= 1:
+        return True
+    else:
+        return word[0] == word[-1] and is_palindrome(word[1:-1])
+
+word = input("Ingresa una cadena: ")
+resultado = is_palindrome(word)
+print(f"¿La cadena es un palíndromo?: {resultado}")
